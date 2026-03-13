dab12c6fb18a01e00f65710dda1e44e65cce22dc
mmorot <mmorot@student.42lyon.fr>
2026-03-13 15:05:25 +0100

    fix: indentation error

--- hints/mouse_service.py
+++ hints/mouse_service.py
@@ -45,125 +45,125 @@ _BUTTON_MAP = {
 }
 
 class Mouse:
-    """Mouse class for performing mouse actions (click, hover, move, etc).
+	"""Mouse class for performing mouse actions (click, hover, move, etc).
 
-    This uses xdotool
-    """
+	This uses xdotool
+	"""
 
-    def __init__(self, abs_max_width=10000, abs_max_height=10000, write_pause=0.03):
-        self.write_pause = write_pause
-        self.abs_max_width = abs_max_width
-        self.abs_max_height = abs_max_height
+	def __init__(self, abs_max_width=10000, abs_max_height=10000, write_pause=0.03):
+		self.write_pause = write_pause
+		self.abs_max_width = abs_max_width
+		self.abs_max_height = abs_max_height
 
-    def scroll(self, x: int, y: int, *_args, **_kwargs):
-         """Scroll event.
+	def scroll(self, x: int, y: int, *_args, **_kwargs):
+		"""Scroll event.
 
-        :param x: X scroll direction.
-        :param y: Y scroll direction. :param *_args: Extra args to use
-            the same interface as move. :param **_kwargs: Extra kwargs
-            to use the same interface as move.
-        """
-        if y < 0:
-            subprocess.run(["xdotool", "click", "4"])  # scroll up
-        elif y > 0:
-            subprocess.run(["xdotool", "click", "5"])  # scroll down
-        if x < 0:
-            subprocess.run(["xdotool", "click", "6"])  # scroll left
-        elif x > 0:
-            subprocess.run(["xdotool", "click", "7"])  # scroll right
+		:param x: X scroll direction.
+		:param y: Y scroll direction. :param *_args: Extra args to use
+			the same interface as move. :param **_kwargs: Extra kwargs
+			to use the same interface as move.
+		"""
+		if y < 0:
+			subprocess.run(["xdotool", "click", "4"])
+		elif y > 0:
+			subprocess.run(["xdotool", "click", "5"])
+		if x < 0:
+			subprocess.run(["xdotool", "click", "6"])
+		elif x > 0:
+			subprocess.run(["xdotool", "click", "7"])
 
-    def move(self, x: int, y: int, absolute: bool = True):
-        """Move event.
+	def move(self, x: int, y: int, absolute: bool = True):
+		"""Move event.
 
-        :param X: X move direction.
-        :param y: Y move direction.
-        :param absolute: Whether to move the mouse using an absolute
-            position.
-        """
-        if absolute:
-            subprocess.run(["xdotool", "mousemove", str(int(x)), str(int(y))])
-        else:
-            subprocess.run(["xdotool", "mousemove_relative", "--", str(int(x)), str(int(y))])
-        sleep(self.write_pause)
+		:param X: X move direction.
+		:param y: Y move direction.
+		:param absolute: Whether to move the mouse using an absolute
+			position.
+		"""
+		if absolute:
+			subprocess.run(["xdotool", "mousemove", str(int(x)), str(int(y))])
+		else:
+			subprocess.run(["xdotool", "mousemove_relative", "--", str(int(x)), str(int(y))])
+		sleep(self.write_pause)
 
-    def click(
-        self,
-        x: int,
-        y: int,
-        button: MouseButton,
-        button_states: Iterable[MouseButtonState],
-        repeat: int = 1,
-        absolute: bool = True,
-    ):
-        """Click event.
+	def click(
+		self,
+		x: int,
+		y: int,
+		button: MouseButton,
+		button_states: Iterable[MouseButtonState],
+		repeat: int = 1,
+		absolute: bool = True,
+	):
+		"""Click event.
 
-        :param x: X position to click.
-        :param y: Y position to click.
-        :param button: Button to use for click.
-        :param actions: Actions to use for the click button (button down
-            / button up).
-        :param repeat: Times to repeat a click.
-        :param absolute: Whether the click position is absolute.
-        """
-        self.move(x, y, absolute=absolute)
-        btn = _BUTTON_MAP.get(button, 1)
-        for _ in range(repeat):
-            for state in button_states:
-                # state 1 = press, 0 = release (evdev KEY_DOWN/KEY_UP)
-                cmd = "mousedown" if state == 1 else "mouseup"
-                subprocess.run(["xdotool", cmd, str(btn)])
-                sleep(self.write_pause)
+		:param x: X position to click.
+		:param y: Y position to click.
+		:param button: Button to use for click.
+		:param actions: Actions to use for the click button (button down
+			/ button up).
+		:param repeat: Times to repeat a click.
+		:param absolute: Whether the click position is absolute.
+		"""
+		self.move(x, y, absolute=absolute)
+		btn = _BUTTON_MAP.get(button, 1)
+		for _ in range(repeat):
+			for state in button_states:
+				# state 1 = press, 0 = release (evdev KEY_DOWN/KEY_UP)
+				cmd = "mousedown" if state == 1 else "mouseup"
+				subprocess.run(["xdotool", cmd, str(btn)])
+				sleep(self.write_pause)
 
-    def do_mouse_action(
-        self,
-        key_press_state: dict[str, Any],
-        key: str,
-        mode: MouseMode,
-    ):
-        """Perform mouse action.
+	def do_mouse_action(
+		self,
+		key_press_state: dict[str, Any],
+		key: str,
+		mode: MouseMode,
+	):
+		"""Perform mouse action.
 
-        :param key_press_state: State containing key press event data
-            used for ramping up speeds.
-        :param key: The key to perform a mouse action for.
-        :param mode: The mouse mode.
-        """
-        sensitivity = 1
-        rampup_time = 1
-        mouse_navigation_action = self.move
-        left, right, up, down = "h", "l", "k", "j"
+		:param key_press_state: State containing key press event data
+			used for ramping up speeds.
+		:param key: The key to perform a mouse action for.
+		:param mode: The mouse mode.
+		"""
+		sensitivity = 1
+		rampup_time = 1
+		mouse_navigation_action = self.move
+		left, right, up, down = "h", "l", "k", "j"
 
-        if mode == MouseMode.MOVE.value:
-            sensitivity = config["mouse_move_pixel_sensitivity"]
-            rampup_time = config["mouse_move_rampup_time"]
-            left = config["mouse_move_left"]
-            right = config["mouse_move_right"]
-            up = config["mouse_move_down"]
-            down = config["mouse_move_up"]
-            mouse_navigation_action = self.move
+		if mode == MouseMode.MOVE.value:
+			sensitivity = config["mouse_move_pixel_sensitivity"]
+			rampup_time = config["mouse_move_rampup_time"]
+			left = config["mouse_move_left"]
+			right = config["mouse_move_right"]
+			up = config["mouse_move_down"]
+			down = config["mouse_move_up"]
+			mouse_navigation_action = self.move
 
-        elif mode == MouseMode.SCROLL.value:
-            sensitivity = config["mouse_scroll_pixel_sensitivity"]
-            rampup_time = config["mouse_scroll_rampup_time"]
-            left = config["mouse_scroll_left"]
-            right = config["mouse_scroll_right"]
-            up = config["mouse_scroll_up"]
-            down = config["mouse_scroll_down"]
-            mouse_navigation_action = self.scroll
+		elif mode == MouseMode.SCROLL.value:
+			sensitivity = config["mouse_scroll_pixel_sensitivity"]
+			rampup_time = config["mouse_scroll_rampup_time"]
+			left = config["mouse_scroll_left"]
+			right = config["mouse_scroll_right"]
+			up = config["mouse_scroll_up"]
+			down = config["mouse_scroll_down"]
+			mouse_navigation_action = self.scroll
 
-        key_press_state.setdefault("sensitivity", sensitivity)
-        if time() - key_press_state["start_time"] >= rampup_time:
-            key_press_state["sensitivity"] += sensitivity
+		key_press_state.setdefault("sensitivity", sensitivity)
+		if time() - key_press_state["start_time"] >= rampup_time:
+			key_press_state["sensitivity"] += sensitivity
 
-        if key == left:
-            mouse_navigation_action(-key_press_state["sensitivity"], 0, absolute=False)
-        if key == right:
-            mouse_navigation_action(key_press_state["sensitivity"], 0, absolute=False)
-        if key == up:
-            mouse_navigation_action(0, key_press_state["sensitivity"], absolute=False)
-        if key == down:
-            mouse_navigation_action(0, -key_press_state["sensitivity"], absolute=False)
+		if key == left:
+			mouse_navigation_action(-key_press_state["sensitivity"], 0, absolute=False)
+		if key == right:
+			mouse_navigation_action(key_press_state["sensitivity"], 0, absolute=False)
+		if key == up:
+			mouse_navigation_action(0, key_press_state["sensitivity"], absolute=False)
+		if key == down:
+			mouse_navigation_action(0, -key_press_state["sensitivity"], absolute=False)
 
-        return key_press_state
+		return key_press_state
 
 
 class MouseService:
